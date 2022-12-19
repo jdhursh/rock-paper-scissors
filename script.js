0b0446fc245264f9323a0a35cf6ac3fa60f908e7
Jesse <jdhursh@eiu.edu>
2022-12-19 10:18:43 -0600

    random comp choice

--- script.js
+++ script.js
@@ -1,73 +1,23 @@
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 9)
 
-function getComputerChoice(){
-let randomNum = Math.floor(Math.random() * 9);
-
-switch(randomNum) {
-    case 0:
-    case 1:
-    case 2:
-        return "Rock";
-        break;
-    case 3:
-    case 4:
-    case 5:
-        return "Paper";
-        break;
-    case 6:
-    case 7:
-    case 8:
-        return "Scissors";
-}
-}
-
-function getPlayerChoice() {
-    let playerInput = prompt ("Rock, Paper, Scissors . . .", "Shoot!");
-    return playerInput;
-}
-
-const playerChoice = getPlayerChoice();
-const compChoice = getComputerChoice();
-
-function playRound(playerChoice, compChoice) {
-    
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-
-    switch(playerChoice) {
-        case "Rock":
-            if (compChoice == "Paper") {
-                return "You lose!";
-            } else if (compChoice == "Rock") {
-                return "Tie!";
-            } else {
-                return "You Win!";
-            }
+    switch(randomNum) {
+        case 0:
+        case 1:
+        case 2:
+            return "rock";
             break;
-        case "Paper":
-            if (compChoice == "Scissors") {
-                return "You lose!";
-            } else if (compChoice == "Paper") {
-                return "Tie!";
-            } else {
-                return "You win!";
-            }
+        case 3:
+        case 4:
+        case 5:
+            return "paper";
             break;
-        case "Scissors":
-            if (compChoice == "Rock") {
-                return "You lose!";
-            } else if (compChoice == "Scissors") {
-                return "Tie!";
-            } else {
-                return "You win!";
-            }
-        default:
-            return "Uh oh! Something went wrong!";
+        case 6:
+        case 7:
+        case 8:
+            return "scissors";
+            break;
     }
 }
-
-let results = playRound(playerChoice, compChoice);
-
-document.getElementById("demo").innerHTML = results;
-document.getElementById("you").innerHTML = "You: " + playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-document.getElementById("comp").innerHTML = "Computer: " + compChoice;
 
 
